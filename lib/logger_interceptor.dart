9811c879e6296cfbb3d5ad513a4de795288caaf6
Atikuzzaman Sabuj <113611885+atik-sabuj@users.noreply.github.com>
2024-10-28 11:33:02 +0600

    Update logger_interceptor.dart

--- lib/logger_interceptor.dart
+++ lib/logger_interceptor.dart
@@ -22,3 +22,37 @@ class LoggerInterceptor extends Interceptor {
     super.onError(err, handler);
   }
 }
+
+
+
+
+
+
+
+
+//new
+
+import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
+
+class LoggerInterceptor extends Interceptor {
+  final Logger _logger = Logger();
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    _logger.i("REQUEST[${options.method}] => PATH: ${options.path} => DATA: ${options.data}");
+    super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    _logger.i("RESPONSE[${response.statusCode}] => DATA: ${response.data}");
+    super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    _logger.e("ERROR[${err.response?.statusCode}] => MESSAGE: ${err.message}");
+    super.onError(err, handler);
+  }
+}
