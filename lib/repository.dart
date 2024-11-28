11cc271456f6dab6246846936476e39eb5f305fe
Atikuzzaman Sabuj <113611885+atik-sabuj@users.noreply.github.com>
2024-11-28 10:56:00 +0600

    Update repository.dart

--- lib/repository.dart
+++ lib/repository.dart
@@ -31,3 +31,21 @@ class ApiRepository {
     });
   }
 }
+
+
+//new
+import 'package:dio/dio.dart';
+import 'locator.dart';
+
+
+class ApiRepository {
+  final DioClient _dioClient = getIt<DioClient>();
+
+  Future<Response> checkInstituteApi() async {
+    return await _dioClient.postRequest('checkInstituteApi', {
+      'apiSecret': 'a2d25cw!3y7w7uk',
+      'loginAs': 'STUDENT',
+      'instituteCode': '116',
+    });
+  }
+}
